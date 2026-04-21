c1214f240b844657f9429af245988e6c1771920b
Lisuccia20 <lisa.manoni2@gmail.com>
2026-04-21 16:18:16 +0200

    prova 2

--- src/frontend/qt_sdl/ScreenStreamer.cpp
+++ src/frontend/qt_sdl/ScreenStreamer.cpp
@@ -1,14 +1,7 @@
 #include "ScreenStreamer.h"
 
-#include <string.h>
+#include <cstring>
 #include <algorithm>
-
-#ifdef _WIN32
-    #include <ws2tcpip.h>
-    #pragma comment(lib, "ws2_32.lib")
-#else
-    #include <unistd.h>
-#endif
 
 #pragma pack(push, 1)
 struct PacketHeader {
@@ -19,25 +12,33 @@ struct PacketHeader {
 #pragma pack(pop)
 
 static constexpr int HEADER_SIZE  = sizeof(PacketHeader);
-static constexpr int CHUNK_PAYLOAD = 60000;
-static constexpr int MAX_PACKET    = HEADER_SIZE + CHUNK_PAYLOAD;
+static constexpr int CHUNK_SIZE   = 60000;
+static constexpr int MAX_PACKET   = HEADER_SIZE + CHUNK_SIZE;
 
 ScreenStreamer::ScreenStreamer(const std::string& ip, uint16_t port) {
 #ifdef _WIN32
-    WSADATA wsaData;
-    WSAStartup(MAKEWORD(2,2), &wsaData);
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2,2), &wsa);
+
+    sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+#else
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
 #endif
 
-    sock = socket(AF_INET, SOCK_DGRAM, 0);
-
     int bufSize = 4 * 1024 * 1024;
-    setsockopt(sock, SOL_SOCKET, SO_SNDBUF, (const char*)&bufSize, sizeof(bufSize));
+    setsockopt(sock, SOL_SOCKET, SO_SNDBUF,
+                reinterpret_cast<const char*>(&bufSize),
+                sizeof(bufSize));
 
-    memset(&addr, 0, sizeof(addr));
+    std::memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_port   = htons(port);
 
+#ifdef _WIN32
+    InetPtonA(AF_INET, ip.c_str(), &addr.sin_addr);
+#else
     inet_pton(AF_INET, ip.c_str(), &addr.sin_addr);
+#endif
 }
 
 ScreenStreamer::~ScreenStreamer() {
@@ -50,29 +51,29 @@ ScreenStreamer::~ScreenStreamer() {
 }
 
 void ScreenStreamer::sendFrame(const void* bgraData, int width, int height) {
-    const uint8_t* data = reinterpret_cast<const uint8_t*>(bgraData);
+    const uint8_t* data = static_cast<const uint8_t*>(bgraData);
 
     int totalBytes  = width * height * 4;
-    int totalChunks = (totalBytes + CHUNK_PAYLOAD - 1) / CHUNK_PAYLOAD;
+    int totalChunks  = (totalBytes + CHUNK_SIZE - 1) / CHUNK_SIZE;
 
     uint8_t packet[MAX_PACKET];
-    PacketHeader* hdr = reinterpret_cast<PacketHeader*>(packet);
+    auto* hdr = reinterpret_cast<PacketHeader*>(packet);
 
     for (int i = 0; i < totalChunks; i++) {
-        int offset      = i * CHUNK_PAYLOAD;
-        int payloadSize = std::min(CHUNK_PAYLOAD, totalBytes - offset);
+        int offset = i * CHUNK_SIZE;
+        int size   = std::min(CHUNK_SIZE, totalBytes - offset);
 
         hdr->frameId     = frameId;
         hdr->chunkIndex  = static_cast<uint16_t>(i);
         hdr->totalChunks = static_cast<uint16_t>(totalChunks);
 
-        memcpy(packet + HEADER_SIZE, data + offset, payloadSize);
+        std::memcpy(packet + HEADER_SIZE, data + offset, size);
 
         sendto(sock,
-               packet,
-               HEADER_SIZE + payloadSize,
+               reinterpret_cast<const char*>(packet),
+               HEADER_SIZE + size,
                0,
-               reinterpret_cast<struct sockaddr*>(&addr),
+               reinterpret_cast<sockaddr*>(&addr),
                sizeof(addr));
     }
 
